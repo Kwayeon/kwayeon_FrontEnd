0b2206560cd91ec3767df69a7b4b5eb282dea749
SunoopDogg <aswoo55555@gmail.com>
2023-08-03 19:44:15 +0900

    feat: Wrap App component with QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { client } from "@/apis/instance";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
